512f6be3dd35208af5b95e2af009e2971c24c991
Gardiano <joaopaulong@hotmail.com>
2024-07-12 14:33:58 -0300

    refactor(pages/home): remove blank spaces

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,10 +1,9 @@
-
 import { Toaster } from '../ui/toaster';
 import { HomeContent } from '../home/homeContent';
 
 export const Home = () => {
   return (
-    <> 
+    <>
       <HomeContent />
       <Toaster />
     </>
